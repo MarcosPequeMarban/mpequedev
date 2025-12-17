b2a2c71da59e30d7e57426bd81040e200b931420
MarcosPequeM <159158821+MarcosPequeMarban@users.noreply.github.com>
2025-12-17 11:31:14 +0100

    Primeros cambios
    Favicon para todas las resoluciones y navbar

---
+++ src/app/shared/navbar/navbar.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrl: './navbar.scss'
+})
+export class Navbar { }
