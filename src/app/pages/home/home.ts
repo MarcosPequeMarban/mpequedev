6a5eb02df92c7d87c836268fe339dea5e2e7db30
MarcosPequeM <159158821+MarcosPequeMarban@users.noreply.github.com>
2025-12-18 11:47:11 +0100

    Home acabada
    Home terminada a expensas de nuevas fotos

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.html',
   styleUrls: ['./home.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
