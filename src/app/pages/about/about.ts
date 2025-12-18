c358f1419b9c05238c2f21d6032c6e16520ecda1
MarcosPequeM <159158821+MarcosPequeMarban@users.noreply.github.com>
2025-12-18 12:27:32 +0100

    sobre mi
    sobre mi todavia a retocar

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'app-about',
-  imports: [],
-  template: `<p>about works!</p>`,
-  styleUrl: './about.css',
+  selector: 'home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './about.html',
+  styleUrls: ['./about.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class About { }
+export class About {}
